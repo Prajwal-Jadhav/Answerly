cceca38e729fc4aea311d42bfe58a1c04223238b
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-06 21:06:32 +0530

    added crispy forms settings

--- Answerly/settings.py
+++ Answerly/settings.py
@@ -33,6 +33,7 @@ AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
