cb52f5c645064e56cdd5854f9869b49e0a194b46
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-06 14:17:32 +0530

    created Answer model

--- main/models.py
+++ main/models.py
@@ -8,6 +8,7 @@ class Question(models.Model):
     This class is used to model questions asked by users
     It has a foreign key which corresponds to a user
     """
+
     title = models.CharField('title for question', max_length=150, blank=False)
     content = models.TextField(
         'more information about question', blank=True, default='')
@@ -17,3 +18,21 @@ class Question(models.Model):
 
     def __str__(self) -> str:
         return self.title[:10] + "..."
+
+
+class Answer(models.Model):
+    """
+    This class is used to model answers given by users to particular question.
+    It has two foreign keys: 1. Question it belongs to 
+                             2. User who wrote it.
+    """
+
+    content = models.TextField(blank=False)
+    question = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    answered_by = models.ForeignKey(
+        to=get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    votes = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.content[:10] + "..."
