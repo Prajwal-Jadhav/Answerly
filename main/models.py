94fdab4435f3ed847d239cc45b948fcbf0201d2d
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-06 13:58:00 +0530

    Created a Question model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from datetime import datetime
 
-# Create your models here.
+
+class Question(models.Model):
+    """
+    This class is used to model questions asked by users
+    It has a foreign key which corresponds to a user
+    """
+    title = models.CharField('title for question', max_length=150, blank=False)
+    content = models.TextField(
+        'more information about question', blank=True, default='')
+    asked_by = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    votes = models.IntegerField('number of votes cast')
+
+    def __str__(self) -> str:
+        return self.title[:10] + "..."
