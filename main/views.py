ffa3a7e419879391bac1054b5e3a35538ab2e187
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-07 12:26:15 +0530

    wrote a view to dispay latest questions

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question
+
+
+def all_questions(request):
+    question_list = Question.objects.order_by('-created_at')[:10]
+
+    return render(request, 'main/all_questions.html', {'question_list': question_list})
