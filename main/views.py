dd2f559f2ed71278361029a7a71e17c865802bbc
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-07 12:53:05 +0530

    added a view to dispay question in detail

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -7,3 +8,12 @@ def all_questions(request):
     question_list = Question.objects.order_by('-created_at')[:10]
 
     return render(request, 'main/all_questions.html', {'question_list': question_list})
+
+
+def question_details(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('question does not exists.')
+
+    return render(request, 'main/question_details.html', {'question': question})
