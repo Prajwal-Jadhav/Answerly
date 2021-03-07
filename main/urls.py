6a0b7e8f801a73482ca711f7f0a1c0b70f2dd0f4
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-07 13:02:24 +0530

    registered question_details view in urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.all_questions, name='all_questions')
+    path('', views.all_questions, name='all_questions'),
+    path('<int:question_id>', views.question_details, name='question_details')
 ]
