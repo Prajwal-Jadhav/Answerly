74eb85ffdd6035e1c1c212e91bfc94eb1269734a
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-08 18:19:30 +0530

    added urlpattern to submit answer

--- main/urls.py
+++ main/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('questions/', views.all_questions, name='all_questions'),
     path('questions/<int:question_id>/',
          views.question_details, name='question_details'),
+    path('questions/<int:question_id>/answers/create',
+         views.create_answer, name='create_answer'),
     path('questions/new', views.create_question, name='create_question')
 ]
