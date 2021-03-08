e43b64c27816683d87a774675237c16988b7adb4
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-08 10:47:34 +0530

    added a view for creating question

--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from .models import Question
+from main.forms import QuestionCreationForm
 
 
 def home(request):
@@ -23,3 +26,18 @@ def question_details(request, question_id):
         raise Http404('question does not exists.')
 
     return render(request, 'main/question_details.html', {'question': question})
+
+
+@login_required
+def create_question(request):
+    if request.method == 'POST':
+        form = QuestionCreationForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.asked_by = request.user
+            question = question.save()
+            redirect(reverse('main:question_details', args=[question.id]))
+    else:
+        form = QuestionCreationForm()
+
+    return render(request, 'main/create_question.html', {'form': form})
