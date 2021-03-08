62448f2183dd997dc2bf21fb490bd6437be3b46b
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-08 17:17:32 +0530

    created form to create answers

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,16 @@
 from django.forms.models import ModelForm
 
 from main.models import Question
+from main.models import Answer
 
 
 class QuestionCreationForm(ModelForm):
     class Meta:
         model = Question
         exclude = ('asked_by', 'created_at', 'votes')
+
+
+class AnswerCreationForm(ModelForm):
+    class Meta:
+        model = Answer
+        exclude = ('answered_by', 'created_at', 'votes')
