8b578d55fe8957999bec572e3e89e89c79767100
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-12 10:52:00 +0530

    added filter horizontal for question vote model in admin page

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Question, Answer, QuestionVote
 
+
+class QuestionVoteAdmin(admin.ModelAdmin):
+    filter_horizontal = ('users_upvoted', 'users_downvoted')
+
+
 admin.site.register(Question)
 admin.site.register(Answer)
-admin.site.register(QuestionVote)
+admin.site.register(QuestionVote, QuestionVoteAdmin)
