4a076bc9e2cd89232ab883ccba853bc9aecbf129
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-12 21:57:22 +0530

    added javascript to handle deleting a vote on the question

--- main/static/main/js/vote.js
+++ main/static/main/js/vote.js
@@ -1,15 +1,29 @@
 const upvote_button = document.getElementById('up');
 const downvote_button = document.getElementById('down');
+const question_votes_count = document.querySelector(".question_votes__count");
 
 data = JSON.stringify({})
 
 function send_voting_request(event) {
-  if (this.dataset.hasvoted == "yes") {
-    console.log("You have already voted on this question.");
-    return;
+  const clicked_button_id = this.getAttribute('id');
+  let action = clicked_button_id;
+
+  if (upvote_button.dataset.hasvoted === 'yes' || downvote_button.dataset.hasvoted === 'yes') {
+
+    let previous_command;
+
+    if (upvote_button.dataset.hasvoted === 'yes'){
+      previous_command = 'up';    
+    } else {
+      previous_command = 'down';
+    }
+
+    if (previous_command === action) {
+      action = 'delete';
+    }
   }
 
-  fetch(`http://localhost:8000/questions/${question_id}/vote/${this.getAttribute('id')}`, {
+  fetch(`http://localhost:8000/questions/${question_id}/vote/${action}`, {
     method: 'POST',
     headers: {
       "X-CSRFToken": csrftoken
