7c61fce2cbcc4f03cbcb71b540b846b75b551a94
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-14 13:17:15 +0530

    registered AnswerVote model to the admin

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Question, Answer, QuestionVote
+from .models import Question, Answer, QuestionVote, AnswerVote
 
 
 class QuestionVoteAdmin(admin.ModelAdmin):
     filter_horizontal = ('users_upvoted', 'users_downvoted')
 
 
+class AnswerVoteAdmin(admin.ModelAdmin):
+    filter_horizontal = ('users_upvoted', 'users_downvoted')
+
+
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(QuestionVote, QuestionVoteAdmin)
+admin.site.register(AnswerVote, AnswerVoteAdmin)
