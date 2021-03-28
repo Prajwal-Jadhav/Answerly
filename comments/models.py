f3877000acbf972c7f701b1c464a26e5d205ab5a
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-03-28 13:50:07 +0530

    corrected import path for Question model in QuestionComment file

--- comments/models.py
+++ comments/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-from ..main.models import Question
+from main.models import Question
 
 
 class QuestionComment(models.Model):
