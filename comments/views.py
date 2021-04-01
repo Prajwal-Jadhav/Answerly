39a2d362d749021cb8b4567baeeecdf0e2c6bf0d
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-04-01 15:42:39 +0530

    Created view function for creating question comment and added its link to question comment form in question_details template

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
-# Create your views here.
+from .forms import QuestionCommentForm
+from main.models import Question
+
+
+@login_required
+def create_question_comment(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    if request.method == 'POST':
+        form = QuestionCommentForm(request.POST)
+
+        if form.is_valid():
+            # since we are going to add question and author manually we
+            # save this form with commit=False
+            comment = form.save(commit=False)
+            comment.question = question
+            comment.author = request.user
+            comment.save()
+
+    return redirect(reverse('main:question_details', args=[question_id]))
