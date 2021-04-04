63e475ab63e9701428724cabf9e0e06e16dc099d
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-04-04 14:06:52 +0530

    created is_moderator method to check in the decorator user_passes_test that a user belongs to the group 'moderators'

--- moderation/views.py
+++ moderation/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+
+def is_moderator(user):
+    """This method is used to create a decorator that gives access to a view only
+        if the user belongs to the group 'moderators'.
+    """
+    if user.groups.filter(name='moderators').exists():
+        return True
+    else:
+        raise PermissionDenied()
+
+
+@login_required
+@user_passes_test(is_moderator)
+def moderation_home(request):
+    return HttpResponse('this is moderation response')
