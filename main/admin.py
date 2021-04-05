78eff451e53c7158cf9588f5950ce6577c8f9548
Prajwal-Jadhav <jadhavv933@gmail.com>
2021-04-05 15:45:43 +0530

    registered QuestionReport and AnswerReport and their custom admins in admin site

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question, Answer, QuestionVote, AnswerVote
+from .models import Question, Answer, QuestionVote, AnswerVote, QuestionReport, AnswerReport
 
 
 class QuestionVoteAdmin(admin.ModelAdmin):
@@ -10,7 +10,17 @@ class AnswerVoteAdmin(admin.ModelAdmin):
     filter_horizontal = ('users_upvoted', 'users_downvoted')
 
 
+class QuestionReportAdmin(admin.ModelAdmin):
+    filter_horizontal = 'reporter',
+
+
+class AnswerReportAdmin(admin.ModelAdmin):
+    filter_horizontal = 'reporter',
+
+
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(QuestionVote, QuestionVoteAdmin)
 admin.site.register(AnswerVote, AnswerVoteAdmin)
+admin.site.register(QuestionReport, QuestionReportAdmin)
+admin.site.register(AnswerReport, AnswerReportAdmin)
